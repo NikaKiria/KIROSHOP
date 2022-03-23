cde62bfa6ead3bc851a2988bbacc7e00278f890f
NikaKiria <knika508@gmail.com>
2022-03-23 19:38:29 +0400

    change table models for mysql db

--- readme/sql_codes/users_table.sql
+++ readme/sql_codes/users_table.sql
@@ -1,14 +1,14 @@
-CREATE TABLE users_table(
+CREATE TABLE users(
     user_id BIGINT UNIQUE AUTO_INCREMENT NOT NULL,
     firstname VARCHAR(50) NOT NULL,
     lastname VARCHAR(50) NOT NULL,
     email VARCHAR(50) UNIQUE NOT NULL,
-    user_password VARCHAR(50) NOT NULL,
+    user_password VARCHAR(1024) NOT NULL,
     id_number BIGINT NOT NULL,
     user_address VARCHAR(64) NOT NULL,
     card_number VARCHAR(50),
     card_validity_period VARCHAR(50),
     cvc_cvv VARCHAR(50),
-    user_create_date TIMESTAMP NOT NULL,
+    user_create_date DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL ,
     PRIMARY KEY (user_id)
 ) AUTO_INCREMENT = 3 CHARSET = utf8
